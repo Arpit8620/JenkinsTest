3a1e92070624e01dd8ee0bcf784b486bf1de734b
root <root@ip-172-31-35-208.ap-south-1.compute.internal>
2022-07-05 16:36:45 +0000

    thirdComm

---
+++ index.js
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div><h1>USER DATA</h1></div>
+        <div><button id="clickbutton">SHOW DATA</button></div>
+    </div>
+    <div>
+        <ul class="unorderedList" id="UlList">
+        </ul>
+    </div>
+    <script>
+        var displayList=[];
+        var dataButton = document.getElementById("clickbutton");
+        var x=0;
+        var dataList = document.getElementById('UlList');
+        var newData=[];
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res)=>{
+            return res.json();
+        })
+        .then((data)=>{
+            newData = data;
+            console.log(data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+
+
+        dataButton.addEventListener('click',()=>{
+            let y=x;
+            for(x=y;x<(y+5) && x<newData.length;x++)
+            {
+                displayList.push(newData[x]);
+                let newText=`${newData[x].id} is the ID and has the name ${newData[x].name} and she is from ${newData[x].address.city}`
+                dataList.innerHTML += `<li>${newText}</li>`
+            }
+        })
+    </script>
+</body>
+</html>
